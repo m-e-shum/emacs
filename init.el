79ab0d37d9ccd3f9a9d153eb9273a895c07c7f13
Shum, Matthew <matthew.shum@novartis.com>
2023-09-12 15:15:49 -0400

    use company for now instead of corfu

--- init.el
+++ init.el
@@ -203,12 +203,6 @@
   ;; Save my spot when I jump to another file
   (advice-add 'ffap :before #'push-mark))
 
-;; Automatic code formatting
-(use-package apheleia
-  :ensure t
-  :config
-  (apheleia-global-mode +1))
-
 
 (use-package highlight-indent-guides
   ;; provides column highlighting.  Useful when you start seeing too many nested
@@ -230,6 +224,33 @@
   :config
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize)))
+
+
+;; Provide drop-down completion.
+(use-package company
+  :ensure t
+  :defer t
+  :custom
+  ;; Search other buffers with the same modes for completion instead of
+  ;; searching all other buffers.
+  (company-dabbrev-other-buffers t)
+  (company-dabbrev-code-other-buffers t)
+  ;; M-<num> to select an option according to its number.
+  (company-show-numbers t)
+  ;; Only 2 letters required for completion to activate.
+  (company-minimum-prefix-length 3)
+  ;; Do not downcase completions by default.
+  (company-dabbrev-downcase nil)
+  ;; Even if I write something with the wrong case,
+  ;; provide the correct casing.
+  (company-dabbrev-ignore-case t)
+  ;; company completion wait
+  (company-idle-delay 0.2)
+  ;; No company-mode in shell & eshell
+  (company-global-modes '(not eshell-mode shell-mode))
+  ;; Use company with text and programming modes.
+    :hook ((text-mode . company-mode)
+           (prog-mode . company-mode)))
 
 ;; Open python files in tree-sitter mode.
 (add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
@@ -327,3 +348,5 @@
     (start-process-shell-command "recollindex"
                                  "*recoll-index-process*"
                                  "recollindex")))
+
+
